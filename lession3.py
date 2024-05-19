96909f2c0b6cb9a5c05604e1a81678a5483faafc
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-05-19 13:17:56 -0400

    lession3

---
+++ lession3.py
@@ -0,0 +1,9 @@
+meaning = 6
+print('')
+# if meaning > 5:
+#     print('Right on!')
+# else:
+#     print('Not Today')
+
+# Ternary Operator
+print('Right on!') if meaning > 5 else print('Not Today')
